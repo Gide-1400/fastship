9b55b736beaa04de89acad322e4d05d707084ba7
Gide-1400 <111868425+Gide-1400@users.noreply.github.com>
2025-10-18 00:05:42 +0300

    Add files via upload

---
+++ fastship/src/components/Hero.jsx
@@ -0,0 +1,9 @@
+// src/components/Hero.jsx
+export default function Hero() {
+  return (
+    <section className="bg-gradient-to-r from-blue-500 to-blue-800 text-white text-center py-20 px-4">
+      <h2 className="text-4xl font-bold mb-4">أوبر للشحنات... لكن بأربعة أحجام!</h2>
+      <p className="text-lg">حوّل المساحات الفارغة إلى دخل، ووفر في شحناتك</p>
+    </section>
+  );
+}
